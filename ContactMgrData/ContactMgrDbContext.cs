892a3494782d76e107e3806efb8c39d8dd0e9f33
PelumiOgunwole <peaksimeon@gmail.com>
2023-03-05 21:50:43 +0100

    Created DBContext Config as a separate module

---
+++ ContactMgrData/ContactMgrDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ContactMgrData
+{
+    public class ContactMgrDbContext:DbContext
+
+    {
+        private static IConfigurationRoot _configuration;
+
+        public ContactMgrDbContext() { }
+
+
+        public ContactMgrDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            _configuration= builder.Build();
+
+            var constr= _configuration.GetConnectionString("ContactMgr");
+            optionsBuilder.UseSqlServer(constr);
+        }
+         
+    }
+}
